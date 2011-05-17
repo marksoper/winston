bf63e4d6519c7972e8ad62c7c9fc06ae2c15d324
Mark Soper <masoper@gmail.com>
2011-05-17 12:35:58 -0400

    adding test for redis counter

---
+++ test/counters/helper.js
@@ -0,0 +1,47 @@
+
+
+var helper = exports;
+var winston = require("winston");
+
+helper.getTransports = function(config) {
+	//
+	// creates winston Transports for each transport in config file
+	//
+	transports = [];
+	for (transport_type in config["transports"]) {
+	  switch(transport_type) {
+	    case "file":
+		    transports.push(new (winston.transports.File)(config["transports"].file));
+			  break;
+		  case "console":
+		    transports.push(new (winston.transports.Console)());
+				break;	
+		  case "loggly":
+			  transports.push(new (winston.transports.Loggly)(config["transports"].loggly));
+				break;
+		  default:
+			  // add warning message
+				break;
+	  }
+  }
+	return transports;
+};
+
+
+helper.getCounters = function(config) {
+	//
+	// creates winston Counters for each counter in config file
+	//
+	counters = [];
+	for (counter_type in config["counters"]) {
+	  switch(counter_type) {
+	    case "redis":
+		    counters.push(new (winston.counters.Redis)(config["counters"].redis));
+			  break;
+		  default:
+			  // add warning message
+				break;
+	  }
+  }
+	return counters;
+};
---
+++ test/counters/test-redis-server.js
@@ -0,0 +1,67 @@
+
+var http = require('http'),
+    fs = require('fs'),
+		sys = require('sys'),
+    path = require('path'),
+		helper = require('./helper');
+
+// using development fork of winston for now
+//require.paths.unshift(path.join(__dirname, '../../..', ''));
+var winston = require('../../winston/lib/winston');
+
+// construct winston transport(s) and counter(s)
+var	configFile = path.join(__dirname, './config/', 'redis-test-config.json'),
+    config = JSON.parse(fs.readFileSync(configFile).toString()),
+		transports = helper.getTransports(config),
+		counters = helper.getCounters(config);
+		
+var logger = new (winston.Logger)({
+		transports: transports,
+		counters: counters
+		});
+		
+// use an instrument to build log metadata from request object
+var instrument = new (winston.instruments.Analog)();
+
+
+exports.createServer = function (port) {
+
+  var server = http.createServer(function (request, response) {
+
+    var body = '';
+
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
+
+    request.on('end', function () {
+		  var response_body = "response body goes here";
+			response.writeHead(200);
+		  response.end(response_body);
+			logger.log("info","log message payload",instrument.transform(request, response, response_body));
+    });
+
+	});
+
+  if (port) {
+    server.listen(port);
+  }
+
+  return server;
+
+};
+
+exports.start = function(options, callback) {
+  var server = exports.createServer(options.collector_port);
+  callback(null, server);
+};
+
+var config = {'collector_port' : 8000};
+
+exports.start(config, function (err, server) {
+  if (err) {
+    return sys.puts('Error starting analog example server: ' + err.message);
+  }
+  sys.puts('analog example server listening on http://127.0.0.1:' + config.collector_port);
+});
+
