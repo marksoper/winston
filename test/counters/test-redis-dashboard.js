9f90c3be819d0c513aee175d6e30e55a68f889b2
Mark Soper <masoper@gmail.com>
2011-05-17 16:22:00 -0400

    dashboard ready to test

--- test/counters/test-redis-dashboard.js
+++ test/counters/test-redis-dashboard.js
@@ -1,18 +1,4 @@
 
-var request = require('request'),
-    path = require('path');
+var rc = require("redis").createClient();
 
-
-var request_options = {
-	uri: 'http://127.0.0.1:8000',
-	method: 'GET',
-	headers: {
-	  referer: 'theonion.com'
-	}
-}
-
-request(request_options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-	  console.log(request_options["method"] + " " + request_options["uri"] + " :: " + body);
-	}
-});
+rc.get("allprop||alltime", redis.print);
