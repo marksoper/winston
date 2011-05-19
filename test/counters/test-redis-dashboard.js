b9256e14204d0bb5468f64be4b3511df6eb92569
Mark Soper <masoper@gmail.com>
2011-05-18 22:36:56 -0400

    new dashboard test w/ count is ready

--- test/counters/test-redis-dashboard.js
+++ test/counters/test-redis-dashboard.js
@@ -5,3 +5,12 @@ var rc = redis.createClient();
 rc.get("allprops||alltime", function(err,reply) {
   console.log("Global Total:  " + reply); 
 });
+
+// using development fork of winston for now
+var winston = require('../../lib/winston');
+
+var	configFile = path.join(__dirname, './config/', 'test-redis-config.json'),
+    config = JSON.parse(fs.readFileSync(configFile).toString()),
+		counter = new (winston.counters.Redis)(config["counters"].redis);
+		
+var counts = counter.count();
