13b82a27d3b7a527660100ae9d410bbc1e5c7239
Mark Soper <masoper@gmail.com>
2011-05-19 23:36:31 +0000

    fixing count

--- test/counters/test-redis-dashboard.js
+++ test/counters/test-redis-dashboard.js
@@ -4,9 +4,12 @@ var redis = require("redis"),
     fs = require("fs");
 var rc = redis.createClient();
 
+
+
 rc.get("allprops||alltime", function(err,reply) {
   console.log("Global Total:  " + reply); 
 });
+
 
 // using development fork of winston for now
 var winston = require('../../lib/winston');
@@ -14,5 +17,7 @@ var winston = require('../../lib/winston');
 var	configFile = path.join(__dirname, './config/', 'test-redis-config.json'),
     config = JSON.parse(fs.readFileSync(configFile).toString()),
 		counter = new (winston.counters.Redis)(config["counters"].redis);
+
+
 		
 var counts = counter.count();
