970c47a59bf0857a1548f94a153ef23d3abcca5e
Mark Soper <masoper@gmail.com>
2011-05-26 21:15:41 +0000

    property vals not getting logged

--- test/counters/test-redis-dashboard.js
+++ test/counters/test-redis-dashboard.js
@@ -16,9 +16,10 @@ var winston = require('../../lib/winston');
 
 var	configFile = path.join(__dirname, './config/', 'test-redis-config.json'),
     config = JSON.parse(fs.readFileSync(configFile).toString()),
-    counter = new (winston.counters.Redis)(config["counters"].redis,rc);
+    counter = new (winston.counters.Redis)(config["counters"].redis,rc),
+    properties = config["counters"].redis["properties"];
 		
-var counts = counter.count(false,false,false,false, function(results) {
+var counts = counter.count(false,false,false,properties, function(results) {
 	for (attr in results) {
 	    console.log("RESULTS: " + attr + " :: " + results[attr]);
 	}
