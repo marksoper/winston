8d0fce8d55f3e9fe5971213cee872ab69eba05f1
Mark Soper <masoper@gmail.com>
2011-06-06 15:15:05 +0000

    testing dashboard

--- test/counters/test-redis-dashboard.js
+++ test/counters/test-redis-dashboard.js
@@ -1,17 +1,7 @@
 
 
-var redis = require("redis"),
-    path = require("path"),
-    fs = require("fs"),
-    events = require('events');
-
-var rc = redis.createClient();
-
-
-
-rc.get("allprops||alltime", function(err,reply) {
-  console.log("Global Total:  " + reply); 
-});
+var path = require("path"),
+    fs = require("fs");
 
 
 // using development fork of winston for now
@@ -19,16 +9,16 @@ var winston = require('../../lib/winston');
 
 var	configFile = path.join(__dirname, './config/', 'test-redis-config.json'),
     config = JSON.parse(fs.readFileSync(configFile).toString()),
-    counter = new (winston.counters.Redis)(config["counters"].redis,rc),
+  counter = new (winston.counters.Redis)(config["counters"].redis),
     properties = config["counters"].redis["properties"];
 		
-console.log(properties);
+
 
 var counts = counter.count(false,false,false,properties, function(results) {
 	for (attr in results) {
 	    console.log("RESULTS: " + attr + " :: " + results[attr]);
 	}
-	counter.quitClient();
+	//counter.quitClient();
 	return;
     });
 
